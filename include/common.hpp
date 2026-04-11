ab5a08baded2fe89b084a37f342d25b77f22e71f
Asendar1 <hamzahtaimour@gmail.com>
2026-04-11 13:20:57 +0300

    Made a common header file and added a config file

---
+++ include/common.hpp
@@ -0,0 +1,12 @@
+enum class HttpResponseCode {
+	OK = 200,
+	BadRequest = 400,
+	NotFound = 404,
+	InternalServerError = 500
+};
+
+enum class HTTPMethod {
+	GET,
+	POST,
+	DELETE
+};
--- main.cpp
+++ main.cpp
@@ -4,11 +4,20 @@ int main(int argc, char* argv[])
 {
 	std::cout << "الشباب هترسب مليون بالمية" << std::endl;
 
+	/* ! the subject says:
+		Your program must use a configuration file, provided as an argument on the com-mand line,
+		or available in a default path.
+		So this check may be unnecessary.
+	*/
 	if (argc < 2)
 	{
 		std::cerr << "Usage: " << argv[0] << " <Config File>" << std::endl;
 		return 1;
 	}
+
+	// * Config parsing
+
+	// * Start the server and listen
 
 	return 0;
 }
